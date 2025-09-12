539381ba282eff092c76a2e87375d71425f10b11
Anand Sogalad <anand.sogalad305@gmail.com>
2025-09-12 14:46:26 +0530

    Add educational explanations on programming concepts
    Introduce a new file with explanations about key programming concepts, including instruction lists, programming languages, source code, compilers, and interpreters. This serves as a foundational guide for understanding basic computer science terminology.

---
+++ module1/section1.py
@@ -0,0 +1,19 @@
+# What is instruction list?
+# A complete set of known commands to be executed by the computer are callled as instruction list.
+
+# What makes a language?
+# Language is made if alphabet, lexis, syntax and semantics
+
+# What is source code?
+# A program written in a programming language is called source code and a file containing source code is called a source file.
+
+# To execute a programs on a computer, they must be translated to machine language.
+# Such translatros are assemblers, compilers, and interpreters.
+
+# Compiler
+# A compiler is used to convert high level programming language into machine language.
+# this is executed once and shared the to any one to execute.
+
+# Interpreter
+# An interpreter is used to execute a program written in a programming language.
+# This is executed every time the program is run or executed.
